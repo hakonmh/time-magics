7febeee893ee07fbab82270793a64af86ff34726
Håkon Magne Holmen <haakonholmen@hotmail.com>
2022-09-20 22:04:26 +0200

    Changed timeit_magic to time_magics in imports

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import random
 from IPython.terminal.embed import InteractiveShellEmbed
-import timeit_magic
+import time_magics as tm
 
 
 def foo(n):
@@ -24,7 +24,7 @@ def _setup_ipython():
 
 
 def test_time():
-    """Tests timeit_magic.time() against the %time magic command"""
+    """Tests tm.time() against the %time magic command"""
     # Setup
     LENGTHS = [10, 1000, 1_000_000]
     ipshell = _setup_ipython()
@@ -32,13 +32,13 @@ def test_time():
     for length in LENGTHS:
         statement = f"foo({length})"
         ipshell.run_line_magic("time", statement)
-        result = timeit_magic.time(statement, globals={'foo': foo})
+        result = tm.time(statement, globals={'foo': foo})
         print(result[-5:])
-        print('')
+        print('------')
 
 
 def test_timeit():
-    """Tests timeit_magic.timeit() against the %timeit magic command"""
+    """Tests tm.timeit() against the %timeit magic command"""
     # Setup
     LENGTHS = [10, 1000, 1_000_000]
     ipshell = _setup_ipython()
@@ -46,8 +46,8 @@ def test_timeit():
     for length in LENGTHS:
         statement = f"foo({length})"
         ipshell.run_line_magic("timeit", statement)
-        timeit_magic.timeit(statement, max_time=5, globals={'foo': foo})
-        print('')
+        tm.timeit(statement, max_time=5, globals={'foo': foo})
+        print('------')
 
 
 def test_time_it():
@@ -57,11 +57,13 @@ def test_time_it():
     ipshell = _setup_ipython()
     # Test
     ipshell.run_line_magic("timeit", f"foo({N})")
-    _foo = timeit_magic.time_it(foo, max_time=5)
+    _foo = tm.time_it(foo, max_time=5)
     _foo(N)
 
 
 if __name__ == "__main__":
     test_time()
+    print()
     test_timeit()
+    print()
     test_time_it()
