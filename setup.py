731406bd3df1582c1f0c62664924df933f087103
Håkon Magne Holmen <haakonholmen@hotmail.com>
2022-10-07 21:43:25 +0200

    Changed version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(current_folder, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='time-magics',
-    version='0.3.0',
+    version='0.4.0',
     description='iPython %time and %timeit magic command in normal Python files',
     long_description=long_description,
     long_description_content_type='text/markdown',
